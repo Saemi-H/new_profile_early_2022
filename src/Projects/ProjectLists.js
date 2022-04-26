c91afd0e1ec5027268d4904127fcf479b4eea590
Saemi Hong <sstyle940104@gmail.com>
2022-04-26 23:09:27 +0900

    add: Add projectlists for instance of refactoring later on

--- src/Projects/ProjectLists.js
+++ src/Projects/ProjectLists.js
@@ -1,3 +1,8 @@
 export const lists = [
-    {title: "Kiosk", description: "QR코드를 통해 웹 뷰 앱으로 이동 가능한 키오스크 앱을 유지 보수 및 React Material 고도화 프로젝트를 진행했습니다.", skills: "React, Redux, React-Query, Leaflet, Material UI, Styled-Component" , address: "https://kiosk.springgo.io/"}
+    {title: "Kiosk", description: "QR코드를 통해 웹 뷰 앱으로 이동 가능한 키오스크 앱을 유지 보수 및 React Material 고도화 프로젝트를 진행했습니다.", 
+    skills: "React, Redux, React-Query, Leaflet, Material UI, Styled-Component" , address: "https://kiosk.springgo.io/"},
+    {title: "Monitor", description: "차량의 상태, 이벤트 상태 등 차량 관련 상태를 관제실에서 볼 수 있는 관제 웹을 React Material 고도화 프로젝트를 진행하고 있습니다.", 
+    skills: "React, Redux, Leaflet, Material UI, Styled Component" , address: '관리자만 접속 가능한 상태입니다.'},
+    {title: "App", description: "Vue.js와 Vuetify로 만들어진 웹 뷰 형식의 앱을 유지 보수 했습니다.", 
+    skills: "Vue, Vuetify" , address: "https://tasioweb.springgo.io/"}
 ]
