a9a1fad996969800458593096ca9fbda1b9139b8
DESKTOP-GTTA5NN\♡홍새미 <sstyle940104@gmail.com>
2024-02-16 14:53:09 +0900

    style: change grettings in walk in page

--- src/WalkIn/WalkIn.js
+++ src/WalkIn/WalkIn.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes } from "styled-components";
 import "../App.css";
 import "./WalkIn.css";
 
 import GalaxyBg from "../images/universe.jpg";
 
-const delay=["안","녕", "하", "세", "요"]
-
+const delay = ["W", "E", "L", "C", "O", "M", "E"];
 
 const waviy = keyframes`
 0%,40%,100% {
@@ -22,57 +21,66 @@ const waviy = keyframes`
 `;
 
 const StyledWaivy = styled.div`
-position: relative;
-top: 20%;
--webkit-box-reflect: below -20px linear-gradient(transparent, rgba(0,0,0,.2));
-font-size: 4.5rem;
+  position: relative;
+  top: 20%;
+  -webkit-box-reflect: below -20px linear-gradient(transparent, rgba(0, 0, 0, 0.2));
+  font-size: 4.5rem;
 
-& > span{
-  opacity: 0;
-        position: relative;
-        display: inline-block;
-        color: #fff;
-        text-transform: uppercase;
-        animation: ${waviy} 3.5s;
-}
-`
-
-const StyledContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
-    position: fixed;
+  & > span {
+    opacity: 0;
+    position: relative;
+    display: inline-block;
+    color: #fff;
+    text-transform: uppercase;
+    animation: ${waviy} 3.5s;
+  }
 `;
 
-
-
+const StyledContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: fixed;
+`;
 
 function WalkIn() {
   const navigate = useNavigate();
   const goToMain = () => {
-    navigate('/main');
-  }
+    navigate("/main");
+  };
   return (
-    <StyledContainer style={{backgroundImage: `url(${GalaxyBg})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", position:"relative"}}>
+    <StyledContainer
+      style={{
+        backgroundImage: `url(${GalaxyBg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        position: "relative",
+      }}
+    >
       <StyledWaivy>
-      {
-        delay.map((num, index) => <span key={index} style={{animationDelay: `calc(.1s * ${index+1})`}}>{num}</span>)
-      }
-      <div className='parent'>
-        <p className='child'>프론트엔드 개발자</p>
+        {delay.map((num, index) => (
+          <span
+            key={index}
+            style={{ animationDelay: `calc(.1s * ${index + 1})` }}
+          >
+            {num}
+          </span>
+        ))}
+        <div className="parent">
+          <p className="child">FrontEnd Developer</p>
         </div>
-        <div className='parent'>
-        <p className='child'>홍새미의 </p>
+        <div className="parent">
+          <p className="child">Saemi's </p>
         </div>
-        <div className='parent'>
-        <p className='child'>홈페이지 입니다</p>
+        <div className="parent">
+          <p className="child">Profile</p>
         </div>
-  </StyledWaivy>
-      <div className='startBtn' onClick={goToMain}>
+      </StyledWaivy>
+      <div className="startBtn" onClick={goToMain}>
         <span>Enter</span>
       </div>
     </StyledContainer>
-  )
+  );
 }
 
 export default WalkIn;
